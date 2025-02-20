87dcb22d0e611e336be0679f269b3e95f392ce14
salahelfatimi <salahfatimi16@gmail.com>
2025-02-20 16:11:51 +0100

    Remove unused video file from Header component

--- components/header/page.jsx
+++ components/header/page.jsx
@@ -24,10 +24,10 @@ export default function Header(){
   
     return (
         <header className=" overflow-hidden h-screen  relative  ">
-            
+
             <motion.div style={{y}} className='relative h-full items-center justify-start flex '>
                 <video className="absolute top-0 left-0 w-full h-full object-cover" autoPlay loop muted>
-                    <source src="/vid/header.mp4" type="video/mp4" />
+                    <source src="/vid/header_vid.mp4" type="video/mp4" />
                 </video>
                 
                 <div className="absolute top-0 left-0 w-full h-full  bg-black opacity-50  "></div>
